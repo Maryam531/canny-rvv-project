7c551c08d9d0dd08972a124111fa2bea0ae540d9
mennasamimoh-sketch <mennasamimoh@gmail.com>
2026-06-19 04:45:41 +0300

    modified bare metal build_riscv

--- build_riscv.sh
+++ build_riscv.sh
@@ -3,23 +3,28 @@ set -e
 
 mkdir -p build-riscv
 
-riscv64-unknown-elf-g++ -static \
+riscv64-unknown-elf-g++ \
+    -static \
+    -no-pie \
+    -Wl,-Ttext=0x80000000 \
     src/main.cpp \
-    src/image_io.cpp \
     src/gaussian.cpp \
     src/sobel.cpp \
     src/magnitude.cpp \
     src/direction.cpp \
     src/nms.cpp \
-    src/nmh.cpp \
     src/threshold.cpp \
     src/rvv_gaussian.cpp \
     src/rvv_magnitude.cpp \
+    src/image_io.cpp \
     src/syscalls.cpp \
     -Iinclude \
     -march=rv64gcv \
     -mabi=lp64d \
-    ${OPT_LEVEL:--O0} \
+    -std=c++17 \
+    ${OPT_LEVEL:--O3} \
+    -ftree-vectorize \
+    -lm \
     -o build-riscv/canny_rv
 
-echo "Build done: ${OPT_LEVEL:--O0}"
+echo "Build done: ${OPT_LEVEL:--O3}"
