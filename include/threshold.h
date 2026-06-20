f38a974b68e81724b18a1088ef4a94e119560757
Logine Ahmed <logineelshazly@gmail.com>
2026-06-20 03:32:22 +0300

    Update threshold.h
    Merge conflict error

--- include/threshold.h
+++ include/threshold.h
@@ -33,8 +33,6 @@ static constexpr uint8_t EDGE_NONE   =   0;
 // Output is used directly as input to hysteresis edge tracing.
 
 void doubleThreshold(
-<<<<<<< HEAD
-
     const uint8_t* input,
 
     uint8_t*       output,
